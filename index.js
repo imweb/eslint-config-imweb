6e3b833d16d09e530f87585217e33eea407eb97c
avwo <avwu@qq.com>
2018-03-12 16:01:16 +0800

    refactor: 兼容老用法

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-element-to-interactive-role': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
     'jsx-a11y/mouse-events-have-key-events': 0,
     'jsx-a11y/no-noninteractive-tabindex': 0,
     'array-callback-return': 0,
